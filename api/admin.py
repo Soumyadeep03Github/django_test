3edebede87e59238fd38fd9bce28698262f232ce
Soumyadeep03Github <52559858+Soumyadeep03Github@users.noreply.github.com>
2020-08-07 03:05:44 +0530

    models created and registered in admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import user, activity_periods
 
 # Register your models here.
+
+admin.site.register(user)
+admin.site.register(activity_periods)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model): 
+    id = models.CharField(primary_key=True, max_length=25)
+    real_name = models.CharField( blank=False, max_length=255)
+    tz = models.CharField( blank=False, max_length=255)
+   
+    def __str__(self):
+        return self.real_name
+
+class activity_periods(models.Model):
+    user_id = models.ForeignKey(user, on_delete = models.CASCADE) 
+    start_time = models.CharField( blank=False, max_length=255)
+    end_time = models.CharField( blank=False, max_length=255)
+   
+    def __str__(self):
+        return self.user_id.real_name +"_"+ str(self.id)
